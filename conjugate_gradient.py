786d395a8830073126c3634d3b0aa2426394513d
siddharth <siddharthswarnkar@gmail.com>
2016-10-18 03:17:24 +0530

    cleaned and renamed conjugate_gradient

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -1,8 +1,7 @@
 import helper as hlp
 import numpy as np
-import math
 
-def conjugateGradient(func, x0, args=(), fprime=None, alpha=0.5, scaling_factor=0.8, numIter=1e5, norm_lim=1e-7, epsilon=1e-10, order=2, disp=True, period=10000):
+def conjugate_gradient(func, x0, args=(), fprime=None, alpha=0.5, scaling_factor=0.8, numIter=1e5, norm_lim=1e-7, epsilon=1e-10, order=2, disp=True, period=10000):
 	if fprime == None:
 		fprime = hlp.compute_numerical_grad(func, len(x0),epsilon)
 
@@ -41,16 +40,7 @@ def conjugateGradient(func, x0, args=(), fprime=None, alpha=0.5, scaling_factor=
 			print("Iter : %d | Function value : %f" %(iters, func_value))
 	return xUpdated
 
-def func(x):        
-	return pow(x[0]-2,2.0)+pow(x[1]-3,4.0) 
 
-def func_grad(x):
-	g = [0,0]
-	g[0] = 2.0*(x[0]-2)
-	g[1] = 4.0*pow(x[1]-3,3.0)
-	return g
 
-grad_func = hlp.compute_numerical_grad(func, 2)
-
-x = conjugateGradient(func, [-4000,7], fprime=func_grad)
-print('\n', x)
+if __name__ == "__main__":
+	pass
