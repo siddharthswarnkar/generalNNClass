bfae8bf313ac5dd3a06a5b0fe9c28d59214133f9
suriya <msuriyak.2495@gmail.com>
2016-10-30 04:12:57 +0530

    Added more tests and class node functions

--- grad_descent.py
+++ grad_descent.py
@@ -6,6 +6,27 @@ import helper as hlp
 def grad_descent(func, x0, args=(), fprime=None, alpha=0.02, adaptive=False,\
 				beta=0.8, numIter=1e5, norm_lim=1e-6, epsilon=1e-10, order=2,\
 				disp=True, period=10000):
+	'''Gradient descent algorithm to optimize the cost function :
+		func = function to be optimized
+		x0   = initial guess
+		args = other arguments to be passed to func
+		fprime = derivative or jacobian of func, if not passed then will be generated automatically
+		alpha  = learning rate
+		adaptive = boolean, allows variable learning rate
+		beta = used only if adaptive is True, back tracking line search
+		numIter = number of iterations
+		norm_lim = minimum value of norm
+		epsilon = delta x for calculting fprime
+		order = order of norm, max value = Inf
+		disp = boolean, displays the iteration number and function value
+		period = period of printing (iteration)
+	Example:
+		def func(x):        
+			return pow(x[0]-2,6.0)+pow(x[1]-3,6.0)
+		x0 = [1,2]
+		point_of_optima = grad_descent(func,x0)
+	'''
+
 	if fprime == None :
 		fprime = hlp.compute_numerical_grad(func, len(x0),epsilon)
 
@@ -32,15 +53,6 @@ def grad_descent(func, x0, args=(), fprime=None, alpha=0.02, adaptive=False,\
 	if disp and iters%period != 0:
 		print("Iteration : %d | Function value : %f" %(iters, func_value))
 	return x	
-
-def func(x):        
-	return pow(x[0]-2,2.0)+pow(x[1]-3,4.0)	
-
-def func_grad(x):
-	g = [0,0]
-	g[0] = 2.0*(x[0]-2)
-	g[1] = 4.0*pow(x[1]-3,3.0)
-	return g
 
 if __name__ == "__main__":
 	pass
