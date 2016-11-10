be8ba8ba46f6c98b77912f17535eb8e58ed51236
suriya <msuriyak.2495@gmail.com>
2016-11-11 04:20:17 +0530

    Added neural network class and Added more functions to node

---
+++ nn.py
@@ -0,0 +1,25 @@
+from conjugate_gradient import conjugate_gradient as cg
+from grad_descent import grad_descent as gd
+import helper as hlp
+import numpy as np
+import node as nd
+import math
+
+class neural_network(object):
+	def __init__(self, num_layers, list_of_layers, activation_func='sigmoid'):
+		self.num_layers = num_layers
+		self.list_of_layers = list_of_layers
+		self.activation_func = activation_func
+		
+		self.nodes = []
+		for i in range(num_layers):
+			if i == 0:
+				temp = [nd.node(inpt=True) for j in range(list_of_layers[i])]
+			elif i != num_layers-1:
+				temp = [nd.node(list_of_layers[i-1], activation_func) for j in range(list_of_layers[i]-1)]
+				temp = [nd.node(bias=True)] + temp
+			else:
+				temp = [nd.node(list_of_layers[i-1], activation_func) for j in range(list_of_layers[i])]			
+
+			self.nodes.append(temp)	
+	
